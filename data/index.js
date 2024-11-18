330f3a5e68af39d77f5c72672505b94423ac2aaa
mbl1000 <markblewis2008@gmail.com>
2024-11-18 08:18:24 -0800

    user-profile page
    Added the profile page of users.  Profile will be the subdocument to users.

--- data/index.js
+++ data/index.js
@@ -1,7 +1,9 @@
 /*
 import * as imageDataFunctions from './images.js'
 import userDataFunctions from './users.js';
+import profileDataFunctions from './profile.js;
 
 export const imageData = imageDataFunctions
 export const userData = userDataFunctions;
+export const profileData = profileDataFunctions;
 */
