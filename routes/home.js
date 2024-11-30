7b4543747660ee85b9cd9f576b73bc9c6e2516fc
Sameer Dharsee <sameer.dharsee90@gmail.com>
2024-11-30 14:54:38 -0500

    Added logout button to home page. Fixed login from login page.

--- routes/home.js
+++ routes/home.js
@@ -4,15 +4,37 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   try {
-    if (req.sesstion.user) {
-      return res.status(200).render('home/home', { title: "NatureScape" , css: "/public/css/home.css", js: "/public/js/home.js"});
-    } else {
-      return res.status(200).render('home/login', { title: "NatureScape" , css: "/public/css/home.css", js: "/public/js/login.js"});
-    }
+    const isUserLoggedIn = !req.session.user;
+    const view = isUserLoggedIn ? 'home/home' : 'home/login';
+    const jsFile = isUserLoggedIn ? '/public/js/home.js' : '/public/js/login.js';
+
+    return res.status(200).render(view, {
+      title: "NatureScape",
+      css: "public/css/home.css",
+      js: jsFile,
+    });
 
   } catch (error) {
-    res.status(500).json({error: error});
+    res.status(500).render('error', {
+      title: 'Error',
+      message: "Something went wrong loading the page",
+      error
+    });
   }
 });
+
+router.get('/logout', (req, res) => {
+  if (req.session.user) {
+    req.session.destroy((err) => {
+      if (err) {
+        console.error('Error logging out: ', err);
+        return res.status(500).send('Internal Server Error');
+      }
+      res.redirect('/login');
+    });
+  } else {
+    res.redirect('/login');
+  }
+}); 
 
 export default router;
