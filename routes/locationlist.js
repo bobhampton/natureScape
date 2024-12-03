1e8a1f6e6669c0ae3bd0b2389a48db0b68c1a3ad
Sameer Dharsee <sameer.dharsee90@gmail.com>
2024-12-03 16:48:10 -0500

    Got viewer working for photos from a location.

--- routes/locationlist.js
+++ routes/locationlist.js
@@ -19,42 +19,53 @@ router.get('/test', async (req, res) => {
 });
 
 router.get('/:locationId', async (req, res) => {
-  //Get location using locationId(req, res) => {
-  const locationCollection = await locations();
-  const locationFound = await locationCollection.findOne({
-    _id: new ObjectId(req.params.locationId)
-  });
+  try {
+    //Get location using locationId(req, res) => {
+    const locationCollection = await locations();
+    const locationFound = await locationCollection.findOne({
+      _id: new ObjectId(req.params.locationId)
+    });
 
-  const location = {
-    _id: locationFound._id,
-    state: locationFound.state,
-    city: locationFound.city,
-    location_name: locationFound.area
-  };
+    const location = {
+      _id: locationFound._id,
+      state: locationFound.state,
+      city: locationFound.city,
+      location_name: locationFound.area
+    };
+    
+    let formattedPhotos;
+    const photoCollection = await photos()
+    const userPhotos = await photoCollection.find(
+      { 'location.location_id': new ObjectId(req.params.locationId) }).toArray();
+    formattedPhotos = userPhotos.map(photo => ({
+      _id: photo._id,
+      photo_name: photo.photo_name,
+      photo_description: photo.photo_description,
+      likes: photo.likes,
+      views: photo.views,
+      img: {
+        data: photo.img.data.toString('base64'),
+        contentType: photo.img.contentType
+      }
+    }));
 
-  const photoCollection = await photos()
-  const userPhotos = await photoCollection.find({ location_id: req.params.locationId }).toArray();
-  const formattedPhotos = userPhotos.map(photo => ({
-    _id: photo._id,
-    photo_name: photo.photo_name,
-    photo_description: photo.photo_description,
-    likes: photo.likes,
-    views: photo.views,
-    img: {
-      data: photo.img.data.toString('base64'),
-      contentType: photo.img.contentType
-    }
-  }));
+    //Render page using location data and photos
+    res.render('locations/location_view_edit', {
+      locationName: location.location_name, 
+      city: location.city, 
+      state: location.state, 
+      images: formattedPhotos, 
+      css: '/public/css/location_view_edit.css', 
+      js: '/public/js/location_view_edit.js'
+    });
 
-  //Render page using location data and photos
-  res.render('locations/location_view_edit', {
-    locationName: location.location_name, 
-    city: location.city, 
-    state: location.state, 
-    images: formattedPhotos, 
-    css: '/public/css/location_view_edit.css', 
-    js: '/public/js/location_view_edit.js'
-  });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Error retrieving images')
+  }
+  
+
+  
 
 })
 //.post()
