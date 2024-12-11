71339738952a925bdfca23480633cc50b6040818
BobbyBoy101 <132041117+BobbyBoy101@users.noreply.github.com>
2024-12-11 01:49:16 -0700

    adding metadata to photos manually

--- tasks/seed.js
+++ tasks/seed.js
@@ -11,22 +11,46 @@ import usersData from '../data/users.js'
 import { seedUsers } from './seedUsers.js'
 import { seedImages } from './seedImages.js'
 import { seedComments } from './seedComments.js'
+import { lookUpRaw, lookUp, lookUpGeoJSON } from 'geojson-places'
+// import wikidataLookup from 'wikidata-entity-lookup'
 
 
 
 const db = await dbConnection()
 await db.dropDatabase()
 
+const lookup = async (latitude, longitude) => {
+  const geoLookUpRaw = await lookUpRaw(latitude, longitude)
+  console.log(geoLookUpRaw)
+}
 
+const test = async () => {
 
+  const photoIndex = 11
+
+  const photosCollection = await photos()
+  const photo = await photosCollection.find({}).toArray()
+
+  let geoLookUpRaw = await lookUpRaw(photo[photoIndex].location.latitude, photo[photoIndex].location.longitude)
+
+  console.log('\n\n***** TESTING *****')
+  console.log('\nphoto name: ', photo[photoIndex].photo_name)
+  //console.log('geoLookUpRaw: ', geoLookUpRaw)
+  console.dir(geoLookUpRaw, {depth: null})
+}
 
 
 const runSeed = async () => {
   await seedUsers()
   await seedImages()
   await seedComments()
-  await closeConnection()
+  
   console.log('Done!')
+
+  //await test()
+
+
+  await closeConnection()
 } //End of runSeed
 
 // Run the seed function (main if you will...)
