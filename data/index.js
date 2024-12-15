d81d99cc948ffb46a7d2c952c4912190962c7095
Srini Gandi <gandi.srini@gmail.com>
2024-12-14 20:12:02 -0500

    cleaning up

--- data/index.js
+++
@@ -1,9 +0,0 @@
-/*
-import * as imageDataFunctions from './images.js'
-import userDataFunctions from './users.js';
-import profileDataFunctions from './profile.js;
-
-export const imageData = imageDataFunctions
-export const userData = userDataFunctions;
-export const profileData = profileDataFunctions;
-*/
